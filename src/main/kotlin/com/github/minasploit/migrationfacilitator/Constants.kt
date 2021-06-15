948f838d2068e507bef5351f9c25b72720ec0184
Minasie Shibeshi <minasploit@gmail.com>
2021-06-15 19:15:11 +0300

    Removed all custom kotlin code

--- src/main/kotlin/com/github/minasploit/migrationfacilitator/Constants.kt
+++
@@ -1,6 +0,0 @@
-package com.github.minasploit.migrationfacilitator
-
-const val PREFIX = "com.github.minasploit.migrationfacilitator."
-const val STARTUP_PROJECT = PREFIX + "startup_project"
-const val DATA_PROJECT = PREFIX + "data_project"
-const val NOTIFICATION_GROUP_ID = "Migration Facilitator"
