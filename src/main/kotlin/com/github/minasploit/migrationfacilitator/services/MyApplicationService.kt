6fa9cc3a92f26d6271a9ad283d485bc2ed416ba6
GitHub Action <action@github.com>
2021-06-15 15:13:39 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/minasploit/migrationfacilitator/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.minasploit.migrationfacilitator.services
+
+import com.github.minasploit.migrationfacilitator.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
